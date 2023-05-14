14ac60eccfc8f49bf56976c8d731ddd4966f2a9e
Ol3xiii <olexitom29@gmail.com>
2023-05-14 17:53:44 +0200

    Modificato il componente Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -63,6 +63,24 @@ export const Card = () => {
             </p>
           </div>
         </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Film 6</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Film 7</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+          </div>
+        </div>
       </div>
       <div className="buttons d-flex justify-content-between">
         <button onClick={() => handleScroll(-100)}>
