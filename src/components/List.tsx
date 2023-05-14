eed6863536828cd83c0330d6d5e6d75430759b16
Ol3xiii <olexitom29@gmail.com>
2023-05-14 22:55:30 +0200

    Inserimento omponente Data

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,17 @@
 import React, { useRef } from 'react';
 import { Card } from './Card';
+import { Data } from '../data/Data';
 
 export const List = () => {
 
   return (
     <div className="list-container">
       <span className="listTitle">Continua a guardare</span>
-      <div className="card-container">
+        <div className="card-container">
           <Card />
+        </div>
+        <div>
+        <Data />
         </div>
       </div>
   );
